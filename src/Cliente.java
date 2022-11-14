0e16df6efd1c37811d6aa83152ea6728e5b80e9a
fernandocastrogon <fernandocastrogon@gmail.com>
2022-11-14 18:55:19 -0300

    Criando classe Cliente

---
+++ src/Cliente.java
@@ -0,0 +1,38 @@
+public class Cliente {
+
+    private String nome;
+    private String email;
+    private String endereco;
+
+    Cliente(String nome, String email, String endereco){
+
+        this.nome = nome;
+        this.email = email;
+        this.endereco = endereco;
+
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+}
