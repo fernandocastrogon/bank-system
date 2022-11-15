b103ea06a4c2f04c6a473f1923683458d7ba0be9
fernandocastrogon <fernandocastrogon@gmail.com>
2022-11-15 19:27:00 -0300

    metodos saca e deposita abstracts, definicao dos gets and setters.

--- src/Conta.java
+++ src/Conta.java
@@ -1,6 +1,41 @@
-public class Conta {
+public abstract class Conta{
 
-    public static void main(String[] args) {
+    private int numero;
+    private int agencia;
+    public double saldo;
+
+
+
+    Conta(Cliente cliente, int numero, int agencia){
+        this.numero = numero;
+        this.agencia = agencia;
+        setCliente(cliente);
+    }
+
+    public void setCliente(Cliente cliente){
 
     }
+
+
+    public abstract boolean saca(double valor);
+
+    public abstract boolean deposita(double valor);
+
+
+    public void setAgencia(int agencia) {
+        this.agencia = agencia;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public int getAgencia() {
+        return agencia;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
 }
