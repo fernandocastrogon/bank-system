c22685fce0fa2d61a3db00e6bca4408820428f03
fernandocastrogon <fernandocastrogon@gmail.com>
2022-11-15 19:26:00 -0300

    Edicao Metodos Saca e Deposita da Conta Corrente, inclusao das taxas e limites no Constructor de Conta Corrente

--- src/ContaCorrente.java
+++ src/ContaCorrente.java
@@ -1,11 +1,43 @@
+import java.util.concurrent.ExecutionException;
+
 public class ContaCorrente extends Conta{
 
     private double limite;
     private double taxa;
 
-    ContaCorrente(Cliente cliente, int numero, int agencia){
+    ContaCorrente(Cliente cliente, int numero, int agencia, double taxa, double limite){
         super(cliente, numero, agencia);
+        this.taxa = taxa;
 
+    }
+
+
+    @Override
+    public boolean saca(double valor) {
+
+        try {
+            super.saldo -= valor + this.taxa;
+        }
+        catch (Exception ex){
+            return false;
+        }
+        return true;
+
+    }
+
+    @Override
+    public boolean deposita(double valor) {
+
+        if (valor < getLimite()) {
+            try {
+                super.saldo += valor;
+            } catch (Exception ex) {
+                return false;
+            }
+            return true;
+        }else {
+            return false;
+        }
     }
 
     public double getLimite() {
